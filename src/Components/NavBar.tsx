13976c94ac023d8fe6a8003e886b6f695a837806
= <=>
2023-09-24 12:16:09 +0100

    modified NavBar.tsx and globals.css

--- src/Components/NavBar.tsx
+++ src/Components/NavBar.tsx
@@ -5,7 +5,7 @@ import React from "react";
 import Link from "next/link";
 
 import { VscClose, VscMenu } from "react-icons/vsc";
-import { BsBell } from "react-icons/bs";
+import { BsBell, BsPersonAdd, BsSearch } from "react-icons/bs";
 import { useGlobalContext } from "@/context/store";
 import { companyName } from "@/lib/variables";
 
@@ -29,25 +29,25 @@ export default function NavBar() {
     }, [])
 
     return (
-        <nav className={`w-full sticky py-2 px-4 z-50 dark:bg-emerald-900 ${scrolled ? '' : ''} fixed top-0 left-0`}>
+        <nav className={`w-full sticky py-2 sm:px-6 px-4 z-50 dark:bg-emerald-900 ${scrolled ? '' : ''} fixed top-0 left-0`}>
             <div className="flex-all my-2">
 
-                <div className="flex-all w-[86px]">
+                <div className="flex-all gap-3 sm:gap-5">
 
                     <button className="text-lg" onClick={toggleMenu}>
                         {isMenuClicked ? <VscClose /> : <VscMenu />}
                     </button>
 
                     <div>
-                        <Link href='/'>{companyName}</Link>
+                        <Link href='/' className="sm:text-lg">{companyName}</Link>
                     </div>
 
                 </div>
 
-                <div className="flex-all gap-4">
-                    <span><input className="border-0 outline-0 rounded w-[100px] p-2 text-sm dark:bg-emerald-700"  placeholder="search"/></span>
+                <div className="flex-all gap-4 sm:gap-5">
+                    <span className="flex items-center gap-2 rounded sm:rounded-tr-xl p-2 text-sm dark:bg-emerald-700"><label htmlFor="search"><BsSearch /></label><input className="border-0 outline-0  bg-transparent w-[90px] sm:w-[180px] " id="search"  placeholder="search"/></span>
                     <button ><BsBell /></button>
-                    <button>auth</button>
+                    <button><BsPersonAdd /></button>
 
                 </div>
 
