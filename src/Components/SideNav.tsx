6d07a5c706cec9608b6d825285b25daec7c581ec
= <=>
2023-09-24 13:12:14 +0100

    modified SideNav.tsx and globals.css

--- src/Components/SideNav.tsx
+++ src/Components/SideNav.tsx
@@ -9,20 +9,20 @@ export default function SideNav() {
     const { isMenuClicked } = useGlobalContext()
 
     return (
-        <div className={`${isMenuClicked ? 'translate-x-0 w-[200px]' : '-translate-x-full w-0'} fixed top-0 bg-white dark:bg-emerald-900 dark:text-emerald-200 text-emerald-700 transition-all ease-linear duration-300 min-h-screen overflow-x-hidden overflow-y-scroll flex justify-center p-4 sm:w-[250px]`}>
+        <div className={`${isMenuClicked ? 'translate-x-0 w-[200px]' : '-translate-x-full w-0'} fixed sm2:relative top-0 bg-white dark:bg-emerald-900 dark:text-emerald-200 text-emerald-700 transition-all ease-linear duration-300 min-h-screen overflow-hidden flex justify-center p-4 sm:w-[250px] sm2:w-[50px] sm2:hover:w-[250px] lg:w-[250px] sm2:m-4 sm2:rounded-md sm2:translate-x-0 group`}>
 
-            <div className='p-2 mt-12  w-[150px] h-full'>
+            <div className='p-2 mt-12 sm2:mt-0 w-[200px] h-full'>
 
-                <ul className='sideNav-Ul'>
-                    <li><BsAlarm /> <Link href='live'>live</Link></li>
-                    <li><BsChevronUp /> <Link href='features'>features </Link></li>
-                    <li><BsChevronUp /> <Link href='sport'>sport </Link></li>
-                    <li><BsBook /> <Link href='blog'>blog</Link></li>
+                <ul className='sideNav-Ul '>
+                    <li><BsAlarm /> <Link href='live' className='sm2:group-hover:flex'>live</Link></li>
+                    <li><BsChevronUp /> <Link href='features' className='sm2:group-hover:flex'>features </Link></li>
+                    <li><BsChevronUp /> <Link href='sport' className='sm2:group-hover:flex'>sport </Link></li>
+                    <li><BsBook /> <Link href='blog' className='sm2:group-hover:flex'>blog</Link></li>
                 </ul>
 
                 <button><BsGear /></button>
                 {/* <GoogleTranslate /> */}
-                
+
             </div>
         </div>
     )
